47319d63d2154ce28ed870fbe27c92c5d60cbb09
Alberto Oliveira <albertooliveira@Air-de-Alberto.home>
2021-12-16 13:05:44 +0000

    Initialized the database, modified the post method to show generated url and added get_or_create_shurt method to the Shurt class

--- smul/models.py
+++ smul/models.py
@@ -9,6 +9,12 @@ from django.shortcuts import get_object_or_404
 BASE = 62
 CHARSET = string.ascii_uppercase + string.ascii_lowercase + string.digits
 
+#from smul.models import Counter
+#>>> c = Counter()
+#>>> c.save()
+#commands used on the shell to initialize Counter on database
+#otherwise counter = get_object_or_404(Counter, pk=1) throws an error
+#because of not existing a Counter object in the database
 
 class Counter(models.Model):
     value = models.IntegerField(null=False, default=1)
@@ -25,3 +31,17 @@ class Counter(models.Model):
 class Shurt(models.Model):
     url = models.URLField(db_index=True, unique=True, null=False)
     code = models.CharField(max_length=10, db_index=True, unique=True, null=False)
+
+    @classmethod
+    def get_or_create_shurt(cls, url):
+        shurt = cls.objects.filter(url=url).first()
+        if shurt:
+            return shurt
+        counter = get_object_or_404(Counter, pk=1)
+        code = counter.encode()
+        counter.value += 1
+        shurt = cls(url=url, code=code)
+        with transaction.atomic():
+            shurt.save()
+            counter.save()
+        return shurt
--- smul/views.py
+++ smul/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import RedirectView, View
+
+import smul
 from .forms import SmulForm
+from .models import Shurt
 
 # Create your views here.
 class IndexView(View):
@@ -13,7 +16,8 @@ class IndexView(View):
 
     def post(self, request):
        form = SmulForm(request.POST)
-       smul="teste"
        if not form.is_valid():
            return render(request, self.template_name, {"form": form})
+       domain = request.build_absolute_uri() #http://127.0.0.1:8000/ in this case
+       smul = domain + Shurt.get_or_create_shurt(form.cleaned_data["url"]).code #encoded/shortened URL
        return render(request, self.template_name, {"form": form, "smul": smul})    
