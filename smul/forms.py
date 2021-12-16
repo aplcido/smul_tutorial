8ed608d069757b765a57ddd27997b11a3709bc84
Alberto Oliveira <albertooliveira@Air-de-Alberto.home>
2021-12-16 12:25:52 +0000

    Added get/post methods, and created the URL form

---
+++ smul/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from django.shortcuts import render
+
+
+class SmulForm(forms.Form):
+    url = forms.URLField(required=True)
+    #,widget=forms.TextInput(attrs={'placeholder': 'Input your URL'})
--- smul/views.py
+++ smul/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import RedirectView, View
+from .forms import SmulForm
 
 # Create your views here.
 class IndexView(View):
     template_name = "smul/index.html"
 
     def get(self, request):
-        return render(request, self.template_name)
+        return render(request, self.template_name, {"form": SmulForm()})
+
+    def post(self, request):
+       form = SmulForm(request.POST)
+       smul="teste"
+       if not form.is_valid():
+           return render(request, self.template_name, {"form": form})
+       return render(request, self.template_name, {"form": form, "smul": smul})    
